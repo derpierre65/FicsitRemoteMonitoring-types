5bd348c92d7c0c6f9d83a03be494a79a2b3b5d9b
derpierre65 <git@derpierre65.de>
2024-10-12 16:02:49 +0100

    feat: add Building enum with power switch & priority power switch

---
+++ src/enums/building.ts
@@ -0,0 +1,8 @@
+enum Building {
+  PowerSwitch = 'Build_PowerSwitch_C',
+  PriorityPowerSwitch = 'Build_PriorityPowerSwitch_C',
+}
+
+export {
+  Building,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export type * from './types/index';
+
+export * from './enums/building';
 export * from './enums/power-slug';
