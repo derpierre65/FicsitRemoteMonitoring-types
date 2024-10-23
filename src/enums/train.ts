b1111054f8e64ed9da2203456571f9f59c1a2f63
derpierre65 <git@derpierre65.de>
2024-10-23 18:30:05 +0100

    feat: add TrainDockingStatus enum

---
+++ src/enums/train.ts
@@ -0,0 +1,12 @@
+enum TrainDockingStatus {
+  Complete = 'Complete',
+  IdleWaitForTime = 'Idle Wait For Time',
+  Loading = 'Loading',
+  None = 'None',
+  Unloading = 'UNloading',
+  WaitForTransferCondition = 'Wait for Transfer Condition',
+  WaitingForTransfer = 'Waiting For Transfer',
+  WaitingToStart = 'Waiting To Start',
+}
+
+export {TrainDockingStatus};
--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export * from './enums/building';
 export * from './enums/entity-type';
 export * from './enums/item-type';
 export * from './enums/power-slug';
+export * from './enums/train';
