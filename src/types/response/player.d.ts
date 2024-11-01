7f7edac19b54ed7b96a70c366bbac18d693dc362
derpierre65 <git@derpierre65.de>
2024-11-01 00:29:12 +0000

    feat: add player inventory

--- src/types/response/player.d.ts
+++ src/types/response/player.d.ts
@@ -2,13 +2,15 @@ import type {
   Features,
   FeaturesProperty,
   HasLocationWIthRotation,
-  IDClassObject
+  IDClassObject,
+  InventoryItemObject
 } from '../general';
 
 type PlayerObject = IDClassObject<'Char_Player_C'> & HasLocationWIthRotation & {
   Online: boolean;
   PlayerHP: number;
   Dead: boolean;
+  Inventory: InventoryItemObject[];
   features: Features<FeaturesProperty<'Player'>>;
 };
 
