a333afe9e478a51f46b20161c9cf9edd5abe7ba8
derpierre65 <git@derpierre65.de>
2024-11-01 00:37:31 +0000

    feat: add TrainStatus enum

--- src/enums/train.ts
+++ src/enums/train.ts
@@ -3,10 +3,17 @@ enum TrainDockingStatus {
   IdleWaitForTime = 'Idle Wait For Time',
   Loading = 'Loading',
   None = 'None',
-  Unloading = 'UNloading',
+  Unloading = 'Unloading',
   WaitForTransferCondition = 'Wait for Transfer Condition',
   WaitingForTransfer = 'Waiting For Transfer',
   WaitingToStart = 'Waiting To Start',
+}
+
+enum TrainStatus {
+  Parked = 'Parked',
+  ManualDriving = 'Manual Driving',
+  SelfDriving = 'Self-Driving',
+  Derailed = 'Derailed',
 }
 
 enum TrainLoadingMode {
@@ -23,5 +30,6 @@ enum TrainLoadingStatus {
 export {
   TrainDockingStatus,
   TrainLoadingMode,
+  TrainStatus,
   TrainLoadingStatus,
 };
